696210d918f85ad41009494c6b2e3f229537ddef
Mudit Bhargava <mbhargava@umass.edu>
2018-02-20 00:14:11 -0500

    initial code for model1

--- code/utils.py
+++ code/utils.py
@@ -1,3 +1,18 @@
 """
 This file will contain helper methods for use across files
 """
+
+# a dictionary that stores the mapping of email_id to id that is used to lookup user embeddings
+user_id_lookup = {}
+
+
+def get_userid(u):
+    """
+    gets the unique user_id for every user. Returns none if the mapping is not present
+    :param u:
+    :return:
+    """
+    if u in user_id_lookup:
+        return user_id_lookup[u]
+    else:
+        return None
