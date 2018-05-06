fb6003a0d03297489e2b4fed706e39f36e8aa413
Mudit Bhargava <mbhargava@umass.edu>
2018-05-05 23:23:03 -0400

    updated code, final plots

--- code/utils.py
+++ code/utils.py
@@ -84,13 +84,20 @@ def plot_with_tsne(labels, embeddings, display_hover=True):
     color_list = assign_labels_colors(labels, colors)
 
     fig, ax = plt.subplots()
-    scatter = ax.scatter(tsne_embs[:,0], tsne_embs[:, 1], c=color_list, s=75)
     if display_hover:
+        scatter = ax.scatter(tsne_embs[:, 0], tsne_embs[:, 1], c=color_list, s=75)
         tooltip = mpld3.plugins.PointLabelTooltip(scatter, labels=labels)
         mpld3.plugins.connect(fig, tooltip)
         mpld3.show()
     else:
+        for col in list(set(color_list)):
+            xc = [tsne_embs[i, 0] for i in range(len(color_list)) if color_list[i] == col]
+            yc = [tsne_embs[i, 1] for i in range(len(color_list)) if color_list[i] == col]
+            lc = [labels[i] for i in range(len(color_list)) if color_list[i] == col]
+            ax.scatter(xc, yc, c=col, s=75, label=lc[0])
+        ax.legend(loc=2, prop={'size': 10})
         outfile = '../outputs/' + constants.RUN_ID + '_tsne-users.png'
+        # plt.show()
         plt.savefig(outfile)
 
 
@@ -301,6 +308,11 @@ def get_dominance_data(email_ids, embs):
                    'Vice President': 4,
                    'President': 5,
                    'CEO': 6}
+    # desgn_order = {'Others': 3,
+    #                'Employee': 3,
+    #                'Manager': 4,
+    #                'Directors': 5,
+    #                'CEO/Presidents': 6}
     user_desgn = load_user_designations(cat='None')
     dom_embs = []
     dom_y = []
@@ -378,6 +390,122 @@ def get_emailid_freq():
         em_freq = pickle.load(f)
     return em_freq
 
+
+def plot_bar_charts(labels, vals, ylabel, title, ymax, baseline=None):
+    # objects = ('CEO/Presidents', 'Employees', 'Directors', 'Managers', 'Others')
+    # objects = ('-1', '0', '+1')
+    plt.rc('ytick', labelsize=15)
+    x_pos = np.arange(len(labels))
+    # acc = [60.47, 48.60, 43.47]
+    if baseline is not None:
+        line_x = np.arange(-1,len(labels)+1)
+        baseline_y = np.zeros(len(line_x))+baseline
+        line, = plt.plot(line_x, baseline_y, color='k', label='Baseline', linewidth=2)
+        plt.legend()
+    plt.bar(x_pos, vals, align='center', width=0.3, color=['#EC7063', '#2ECC71', '#3498DB']) #color='#3498DB')
+    plt.xticks(x_pos, labels)
+    plt.ylabel(ylabel)
+    plt.ylim((0, ymax))
+    # plt.title('Dominance')
+    plt.title(title)
+    plt.tight_layout()
+
+    outfile = '../outputs/' + title + '.png'
+    # plt.show()
+    plt.savefig(outfile)
+
+
+def plot_bar_charts_v2(labels, vals, ylabel, title, ymax, display_plot=False):
+    cols = vals.shape[0]
+    N = 2 * vals.shape[1] + 1
+
+    colours = ['#EC7063', '#2ECC71', '#3498DB']
+    ind = np.arange(N)  # the x locations for the groups
+    width = 0.35  # the width of the bars
+
+    plt.rc('ytick', labelsize=15)
+    fig, ax = plt.subplots()
+
+    legend_colours = []
+    for col in xrange(cols):
+        this_vals = np.zeros(N)
+        this_vals[1:N:2] = vals[col, :]
+        this_rects = ax.bar(ind + col * width, this_vals, width, color=colours[col % len(colours)])
+        legend_colours.append(this_rects[0])
+
+    ax.set_ylabel(ylabel)
+    ax.set_title(title)
+    ax.set_xticks(ind + width)
+    ax.set_xticklabels(('', 'Positive', '', 'Negative', ''))
+
+    ax.legend(legend_colours, labels)
+
+    plt.ylim((0, ymax))
+    plt.tight_layout()
+
+    if display_plot:
+        plt.show()
+    else:
+
+        outfile = '../outputs/' + title + '.png'
+        plt.savefig(outfile)
+
+
+
+# labels = ['SR Model', 'PV Model', 'Discriminative']
+# ymax = 1.0
+#
+# title = 'Hits @ 1000'
+# vals = np.array([[0.57, 0.38],
+#                    [0.74, 0.50],
+#                    [0.529, 0.514]])
+# ylabel = 'Hit ratio'
+# plot_bar_charts_v2(labels, vals, ylabel, title, ymax, display_plot=False)
+#
+# title = 'AP @ 1000'
+# vals = np.array([[0.30, 0.16],
+#                    [0.58, 0.28],
+#                    [0.29, 0.28]])
+# ylabel = 'Average Precision'
+# plot_bar_charts_v2(labels, vals, ylabel, title, ymax, display_plot=False)
+
+
+# labels = ['SR Model','PV Model','Discriminative']
+# vals = [60.47, 48.60, 54.94]
+# ylabel = 'Accuracy'
+# title = 'Accuracy for Hierarchy Relations'
+# plot_bar_charts(labels, vals, ylabel, title, ymax=100.0, baseline=37.64)
+
+# labels = ['Lower','Equal','Higher']
+# vals = [36.12, 26.23, 37.64] # 4186
+# ylabel = 'Percentage Distribution'
+# title = 'Class Distribution for Hierarchy Relation'
+# plot_bar_charts(labels, vals, ylabel, title, ymax=50, baseline=None)
+
+# labels = ['SR Model','PV Model','Discriminative']
+# vals = [48.69, 43.47, 42.6]
+# ylabel = 'Accuracy'
+# title = 'Accuracy for Role Prediction'
+# plot_bar_charts(labels, vals, ylabel, title, ymax=100.0, baseline=34.78)
+
+# labels = ['CEO/Presidents','Directors','Employee', 'Manager', 'Others']
+# vals = [26.95, 13.91, 34.78, 12.17, 12.17] # [31, 16, 40, 14, 14]
+# ylabel = 'Percentage Distribution'
+# title = 'Class Distribution for Role Prediction'
+# plot_bar_charts(labels, vals, ylabel, title, ymax=50, baseline=None)
+
+# labels = ['CEO/Presidents','Directors','Employee', 'Manager', 'Others']
+# vals = [0.67, 0.0, 0.56, 0.0, 0.0]
+# ylabel = 'F-score'
+# title = 'Class-wise F-score for Role Prediction'
+# plot_bar_charts(labels, vals, ylabel, title, ymax=1.0, baseline=None)
+
+# labels = ['Lower','Equal','Higher']
+# vals = [0.72, 0.20, 0.71]
+# ylabel = 'F-score'
+# title = 'Class-wise F-score for Hierarchy Relation'
+# plot_bar_charts(labels, vals, ylabel, title, ymax=1.0, baseline=None)
+
 # em_freq = get_emailid_freq()
 # email_ids, embs = load_user_embeddings('../important_embeddings/usr50d_em50d_m2faster_20ep/embeddings_usr50d_em50d_m2faster_20ep.pkl')
 # designations = load_user_designations(cat='None')
@@ -388,6 +516,6 @@ def get_emailid_freq():
 # get_user_split(list(email_set), em_freq)
 # x, y = get_dominance_data(email_ids, embs)
 # k_fold_cross_validation(email_ids, embs)
-# email_ids, embs = load_user_embeddings('../important_embeddings/embeddings_usr50d_em100d_custom_25ep_m3/embeddings_usr50d_em100d_custom_25ep_m3.pkl')
+# email_ids, embs = load_user_embeddings('../important_embeddings/usr100d_em200d_25ep_m2faster/embeddings_usr100d_em200d_25ep_m2faster.pkl')
 # embs, labels = extract_emb_desgn(email_ids, embs, cat='cat1')
-# plot_with_tsne(labels.tolist(), embs)
+# plot_with_tsne(labels.tolist(), embs, display_hover=False)
