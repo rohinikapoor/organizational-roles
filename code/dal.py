330013c3dafae88c1a0656cc8f947a09074c8364
mohit-surana <mohit.surana95@gmail.com>
2018-05-09 00:53:58 -0400

    Exploratory analysis

--- code/dal.py
+++ code/dal.py
@@ -68,6 +68,25 @@ def dataset_split(data, val_split=0.0, test_split=0.3):
     return train_data, val_data, test_data
 
 
+def dataset_filter_by_user(data, val_split=0.0, test_split=0.3, threshold=1):
+    train, val, test = [], [], []
+    mails_by_senders = utils.group_mails_by_sender(data)
+    for sender in mails_by_senders:
+        mails = mails_by_senders[sender]
+
+        train_split = int((1 - val_split - test_split) * len(mails))
+        val_split = train_split + int(val_split * len(mails))
+
+        mails = np.array(sorted(mails, key=lambda x: x[3]))
+        train.append(mails[: train_split])
+
+        if train_split > threshold:
+            val.append(mails[train_split: val_split])
+            test.append(mails[val_split:])
+
+    return np.concatenate(train), np.concatenate(val), np.concatenate(test)
+
+
 def get_negative_emails(emails, fraction=1.0):
     """
     Get examples of mails not sent by sender and receiver pairs as negative examples for classification
@@ -85,7 +104,7 @@ def get_negative_emails(emails, fraction=1.0):
 
     num_examples = min(int(len(emails) * fraction), len(data))
     random_state = random.getstate()
-    random.seed(42)
+    random.seed(47)
     random.shuffle(data)
     data = data[:num_examples]
     i = 0
--- code/main.py
+++ code/main.py
@@ -45,7 +45,8 @@ if __name__ == '__main__':
     emails = dal.get_emails_by_users(num_users=num_users)
     print 'Number of emails returned by dal', len(emails)
 
-    train, val, test = dal.dataset_split(emails, val_split=0.1, test_split=0.2)
+    # train, val, test = dal.dataset_split(emails, val_split=0.1, test_split=0.2)
+    train, val, test = dal.dataset_filter_by_user(emails, val_split=0.1, test_split=0.2, threshold=5)
 
     # w2v.train(emails)
 
@@ -81,27 +82,48 @@ if __name__ == '__main__':
     # plt.plot(error_invalid)
     # plt.savefig('../outputs/error-invalid.png')
 
-    mails_grouped_by_sender = utils.group_mails_by_sender(train)
-    mails = mails_grouped_by_sender['j.kaminski@enron.com'][:100]
-    neg_mails = dal.get_negative_emails(mails, fraction=1.0)
-    y_true, y_pred = metrics_utils.get_predictions(model, w2v, mails, neg_mails, is_l2=True)
-
+    # mails_grouped_by_sender = utils.group_mails_by_sender(train)
+    # mails = mails_grouped_by_sender['j.kaminski@enron.com'][:100]
+    # neg_mails = dal.get_negative_emails(mails, fraction=1.0)
+    # y_true, y_pred = metrics_utils.get_predictions(model, w2v, mails, neg_mails, is_l2=True)
+    #
     import matplotlib.pyplot as plt
-
-    error_valid = y_pred[y_true == 1]
-    error_invalid = y_pred[y_true == 0]
-    error_valid = error_valid[error_valid.argsort()]
-    error_invalid = error_invalid[error_invalid.argsort()]
-
-    plt.close()
-    plt.plot(error_valid)
-    plt.savefig('../outputs/error-valid.png')
-
-    plt.plot(error_invalid)
-    plt.savefig('../outputs/error-invalid.png')
+    #
+    # error_valid = y_pred[y_true == 1]
+    # error_invalid = y_pred[y_true == 0]
+    # error_valid = error_valid[error_valid.argsort()]
+    # error_invalid = error_invalid[error_invalid.argsort()]
+    #
+    # plt.close()
+    # plt.plot(error_valid)
+    # plt.savefig('../outputs/error-valid.png')
+    #
+    # plt.close()
+    # plt.plot(error_invalid)
+    # plt.savefig('../outputs/error-invalid.png')
 
     # metrics.evaluate_metrics(model, model_name, w2v, test, neg_emails, k=1000,
     #                          metrics=['hits@k', 'ap@k', 'map@k', 'ryan-hits@k'])
     # metrics.evaluate_metrics(model, model_name, w2v, test, neg_emails, k=1000, metrics=['hits@k', 'ap@k'])
+
+    train_mails_grouped_by_sender = utils.group_mails_by_sender(train)
+    val_mails_grouped_by_sender = utils.group_mails_by_sender(val)
+    test_mails_grouped_by_sender = utils.group_mails_by_sender(test)
+    for sender in val_mails_grouped_by_sender:
+        _, train_errors = metrics_utils.get_predictions(model, w2v, train_mails_grouped_by_sender[sender],
+                                                        neg_emails=[], is_l2=True)
+        _, val_errors = metrics_utils.get_predictions(model, w2v, val_mails_grouped_by_sender[sender],
+                                                      neg_emails=[], is_l2=True)
+        _, test_errors = metrics_utils.get_predictions(model, w2v, test_mails_grouped_by_sender[sender],
+                                                       neg_emails=[], is_l2=True)
+        plt.close()
+        x = [train_errors, val_errors, test_errors]
+        plt.hist(x, 10, histtype='bar', color=['b', 'g', 'r'])
+        # plt.hist(train_errors, histtype='step', stacked=True, fill=False, color='b')
+        # plt.hist(val_errors, histtype='step', stacked=True, fill=False, color='g')
+        # plt.hist(test_errors, histtype='step', stacked=True, fill=False, color='r')
+
+        plt.title(sender)
+        plt.savefig('../outputs/l2-errors/{}.png'.format(sender))
 
     print 'End of script! Time taken ' + str(time.time() - start)
