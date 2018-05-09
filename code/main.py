d9727f8456307441d93a072a82dd9e1b21c4787f
mohit-surana <mohit.surana95@gmail.com>
2018-05-09 13:07:55 -0400

    Threshold users based on number of training emails User based train-val-test split

--- code/main.py
+++ code/main.py
@@ -44,7 +44,8 @@ if __name__ == '__main__':
     emails = dal.get_emails_by_users(num_users=num_users)
     print 'Number of emails returned by dal', len(emails)
 
-    train, val, test = dal.dataset_split(emails, val_split=0.1, test_split=0.2)
+    # train, val, test = dal.dataset_split(emails, val_split=0.1, test_split=0.2)
+    train, val, test = dal.dataset_filter_by_user(emails, val_split=0.1, test_split=0.2, threshold=25)
 
     # w2v.train(emails)
 
